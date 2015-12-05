ab758a4f466cc44a389eba0da90f9ac41119cd0c
Runagar <runagar@gmail.com>
2015-12-05 23:58:36 +0100

    Set up the initial files needed for the program

---
+++ src/miniproject/ClientHandlerOut.java
@@ -0,0 +1,28 @@
+package miniproject;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+// Class for handling the server-output which is sent to "ClientReceiver" class.
+
+public class ClientHandlerOut extends Thread {
+	
+//	TODO: Initialise private variables: Socket, PrintWriter
+	
+//	Constructor:
+	public ClientHandlerOut(Socket socket){
+//		TODO: Define socket.
+//		TODO: Set up printWriter to outputStream
+	}
+	
+	public void run(){
+//		TODO: Compare words in the String to previous words.
+//		HINT: Save new words as Nodes in an arrayList. Compare new words to all words in the list 
+//		before making nodes out of them. We don't want the same word as two separate nodes.
+		
+//		TODO: Sort the arrayList so that words with higher occurrences comes first, using any sorting algorithm you wish.
+		
+//		TODO: Send arrayList of words as String to ClientReceiver via PrintWriter.
+	} 
+}
---
+++ src/miniproject/ClientReceiver.java
@@ -0,0 +1,36 @@
+package miniproject;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ClientReceiver {
+	
+//	TODO: Set up port and server address.
+	
+	public static void main(String[] args){
+		
+//		TODO: Connect to host.
+		receiveMessages();
+	}
+	
+	public static void receiveMessages(){
+		
+//		TODO: Initialise socket.
+		
+		try{
+//			TODO: Define socket.
+//			TODO: Set up Scanner (inputStream)
+			
+			do{
+				
+//				TODO: Set string to nextList. (Scanner)
+//				TODO: Print string to console.
+				
+			}while(true);
+			
+		}catch(IOException ioEx){
+			ioEx.printStackTrace();
+		}
+	}
+}
