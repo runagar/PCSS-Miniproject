bfcf3012990107851825bb1cb22ff8575e2b7348
Runagar <runagar@gmail.com>
2015-12-09 12:52:42 +0100

    indent

--- src/miniproject/Server.java
+++ src/miniproject/Server.java
@@ -8,43 +8,24 @@ import java.util.*;
 
 public class Server {
 	
-//	TODO: Set up Port and ServerSocket
-	public static ServerSocket serverserver;
-	public static final int PORT=15333;
-	
-	public static void main(String[] args) throws IOException{
-try{	
-	serverserver = new ServerSocket(PORT);	
-}
-	catch(IOException ioEx){
-		System.out.println("\nUnable to set up port.");
-		System.exit(1);
-	}
-do{
-	Socket system = serverserver.accept();
-	System.out.println("\nYou are connected to the specific port!");
-	ClientHandlerIn getmyinfo = new ClientHandlerIn(system);
-	getmyinfo.start();
-	
-}while(true);
-	
-	
-//		TODO: Try/catch: Connect serverSocket to port.
+	//	TODO: Set up Port and ServerSocket
+		public static ServerSocket serverserver;
+		public static final int PORT=15333;
 		
-//		TODO: do/while(true): Wait for clients to connect, then start ClientHandlerIn thread
+		public static void main(String[] args) throws IOException{
+	try{	
+		serverserver = new ServerSocket(PORT);	
 	}
-//	public void ClientOut(){
-//		try{
-//			serverserver=new ServerSocket(PORT);
-//		} 
-//		catch(IOException ioEx){
-//			System.out.println("\nCannot connected to the server on the specific port!");
-//			System.exit(1);
-//		}
-//		do{
-//		Socket system = serverserver.accept();
-//		ClientHandlerOut whatshouldmacallit = new ClientHandlerOut(system);
-//		whatshouldmacallit.start();
-//	}while(true);
-//	TODO: Start ClientHandlerOut thread. Talk with whoever is programming that part, to figure out how you wanna do this.
+		catch(IOException ioEx){
+			System.out.println("\nUnable to set up port.");
+			System.exit(1);
+		}
+	do{
+		Socket system = serverserver.accept();
+		System.out.println("\nYou are connected to the specific port!");
+		ClientHandlerIn getmyinfo = new ClientHandlerIn(system);
+		getmyinfo.start();
+		
+		}while(true);
+	}
 }
