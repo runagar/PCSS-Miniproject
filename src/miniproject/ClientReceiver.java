e9f1b7ec592d99a6e39e119cd28fe9821e040907
Jette Møller Jensen <mqller.calico@gmail.com>
2015-12-09 14:02:34 +0100

    this has been done and created and made

--- src/miniproject/ClientReceiver.java
+++ src/miniproject/ClientReceiver.java
@@ -4,35 +4,41 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
-// ClientReceiver class. Receives messages from the server.
+// ClientReceiver class. Receives messages from the server. 
 
-public class ClientReceiver {
+public class ClientReceiver {	
+//	Setting up port and server address.
+	static Socket socket;											//creating new Socket Object
+	static InetAddress link;										//declaring InetAddress called link
+	static final int port = 15333;									//initializing port to 15333
+	static Scanner inputScanner; 									//declaring Scanner
+	static String input;											//declaring String 'input'. 
 	
-//	TODO: Set up port and server address.
-	
-	public static void main(String[] args){
-		
-//		TODO: Connect to host.
-		receiveMessages();
+	public static void main(String[] args){							//main method
+		//finding host and connecting
+		try{														//try catch function
+			link = InetAddress.getLocalHost();						//link = ...
+		}catch(UnknownHostException uhEx){							//if this can't then, system is caught by UnknownHostException
+			System.out.println("Host not found!");					//prints out "..." in the console. 
+		}
+		receiveMessages();											//calls method 'receiveMessages'
 	}
 	
 	public static void receiveMessages(){
 		
-//		TODO: Initialise socket.
-		
 		try{
-//			TODO: Define socket.
-//			TODO: Set up Scanner (inputStream)
+			socket = new Socket(link,port);							//initializing socket.
+			inputScanner = new Scanner(socket.getInputStream()); 	//Setting up Scanner to listen for anything that happens in the socket.
 			
 			do{
-				
-//				TODO: Set string to nextList. (Scanner)
-//				TODO: Print string to console.
+				input = inputScanner.nextLine();					//input is set to be whatever the Scanner gets. 
+				System.out.println(input);							//then prints the String 'input' into the console.
 				
 			}while(true);
 			
-		}catch(IOException ioEx){
-			ioEx.printStackTrace();
+		}catch(IOException ioEx){									//the system is caught by an IOException.
+			ioEx.printStackTrace();									//prints what happens.
+			System.exit(1);											//exits system - 1 means that the exit of the system was unintended.
 		}
 	}
 }
