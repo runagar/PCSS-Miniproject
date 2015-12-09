fe53f21c84f0ef2805058f1675247e4c7650ca3e
Yoh874 <micuks1@gmail.com>
2015-12-09 10:35:33 +0100

    look at it now

--- src/miniproject/Server.java
+++ src/miniproject/Server.java
@@ -30,19 +30,19 @@ do{
 		
 //		TODO: do/while(true): Wait for clients to connect, then start ClientHandlerIn thread
 	}
-	public void ClientOut(){
-		try{
-			serverserver=new ServerSocket(PORT);
-		} 
-		catch(IOException ioEx){
-			System.out.println("\nCannot connected to the server on the specific port!");
-			System.exit(1);
-		}
-		do{
-		Socket system = serverserver.accept();
-		ClientHandlerOut whatshouldmacallit = new ClientHandlerOut(system);
-		whatshouldmacallit.start();
-	}while(true);
+//	public void ClientOut(){
+//		try{
+//			serverserver=new ServerSocket(PORT);
+//		} 
+//		catch(IOException ioEx){
+//			System.out.println("\nCannot connected to the server on the specific port!");
+//			System.exit(1);
+//		}
+//		do{
+//		Socket system = serverserver.accept();
+//		ClientHandlerOut whatshouldmacallit = new ClientHandlerOut(system);
+//		whatshouldmacallit.start();
+//	}while(true);
 	
 //	TODO: Start ClientHandlerOut thread. Talk with whoever is programming that part, to figure out how you wanna do this.
 	}
